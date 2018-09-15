c99c6ad28e3677f63d2489451f041bc4aac20316
Ning <dapao_92@hotmail.com>
2018-09-16 00:00:50 +0800

    Tag

---
+++ app/src/main/java/com/example/samliew/swoosh/BaseActivity.kt
@@ -0,0 +1,45 @@
+package com.example.samliew.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class BaseActivity : AppCompatActivity() {
+
+    val TAG = "LifeCycle"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"${javaClass.simpleName} on CREATE")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(TAG,"${javaClass.simpleName} on START")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG,"${javaClass.simpleName} on RESUME")
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG,"${javaClass.simpleName} on Restart")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        Log.d(TAG,"${javaClass.simpleName} on Pause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(TAG,"${javaClass.simpleName} on Stop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG,"${javaClass.simpleName} on Destroy")
+        super.onDestroy()
+    }
+}
