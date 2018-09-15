105db051db576faa0483b5d363da6a93fa7b6c4b
Ning <dapao_92@hotmail.com>
2018-09-15 23:12:43 +0800

    intent lession

--- app/src/main/java/com/example/samliew/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/samliew/swoosh/WelcomeActivity.kt
@@ -1,12 +1,23 @@
+
 package com.example.samliew.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedButton.setOnClickListener{
+            // two types of intent
+            // 1. implicit intent -  communicate to android and gives you the option
+            // 2. explicit intent -  communicate with specified java class
+            val leagueIntent = Intent(this,LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
